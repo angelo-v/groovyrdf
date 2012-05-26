aaed5afa11f403a28abbc0fa3282c0b8fcd12332
Angelo Veltens <angelo.veltens@online.de>
2012-05-27 00:01:08 +0200

    - groovy 1.8 call style - RdfResource: return literal without defined language, even if language is requested - added tests

--- src/main/groovy/de/datenwissen/util/groovyrdf/jena/JenaRdfResource.groovy
+++ src/main/groovy/de/datenwissen/util/groovyrdf/jena/JenaRdfResource.groovy
@@ -9,79 +9,80 @@ import de.datenwissen.util.groovyrdf.core.RdfBuilder;
 import de.datenwissen.util.groovyrdf.core.RdfResource
 
 class JenaRdfResource implements RdfResource {
-	
-	Resource jenaResource
-	
-	public JenaRdfResource(Resource resource) {
-		this.jenaResource = resource
-	}
-	
-	String getUri() {
-		jenaResource.getURI()
-	}
-	
-	String getType() {
-		return jenaResource.getProperty(RDF.type).getResource().getURI()
-	}
-	
-	Set<String> listProperties() {
-		def statements = jenaResource.listProperties().toList()
-		def result = [] as Set
-		statements.each {
-			result += it.getPredicate().getURI()
-		}
-		result.remove(RDF.type.getURI())
-		return result
-	}
-	
-	def propertyMissing(String propertyUri){
-		return getRdfProperty(propertyUri, null)
-	}
-	
-	def call(String propertyUri) {
-		return getRdfProperty(propertyUri, null)
-	}
-	
-	def call(String propertyUri, String language) {
-		getRdfProperty(propertyUri, language)
-	}
-	
-	private getRdfProperty(String propertyUri, String language) {
-		
-		def property = ResourceFactory.createProperty(propertyUri)
-		def statements = jenaResource.listProperties(property).toList()
-		
-		def set = getPropertiesSet(statements, language)
-		if (set.size() > 1 ) {
-			return set
-		} else {
-			if (set.isEmpty()) return null
-			return set.asList().first()
-		}
-	}
-	
-	private getPropertiesSet(List statements, String language) {
-		def set = [] as Set
-		statements.each { statement ->
-			def value = getLiteralValueOrResource(statement, language)
-			if (value) set += value 
-		}
-		return set 
-	}
-	
-	private getLiteralValueOrResource(Statement statement, String language) {
-		def node = statement.getObject()
-		if (node.isLiteral()) {
-			if (!language || node.asLiteral().getLanguage() == language) {				
-				return node.asLiteral().getValue()
-			}
-		} else {
-			return new JenaRdfResource(node.asResource())
-		}
-	}
-	
-	String toString() {
-		return uri
-	}
-	
+
+    Resource jenaResource
+
+    public JenaRdfResource (Resource resource) {
+        this.jenaResource = resource
+    }
+
+    String getUri () {
+        jenaResource.getURI ()
+    }
+
+    String getType () {
+        return jenaResource.getProperty (RDF.type).getResource ().getURI ()
+    }
+
+    Set<String> listProperties () {
+        def statements = jenaResource.listProperties ().toList ()
+        def result = [] as Set
+        statements.each {
+            result += it.getPredicate ().getURI ()
+        }
+        result.remove (RDF.type.getURI ())
+        return result
+    }
+
+    def propertyMissing (String propertyUri) {
+        return getRdfProperty (propertyUri, null)
+    }
+
+    def call (String propertyUri) {
+        return getRdfProperty (propertyUri, null)
+    }
+
+    def call (String propertyUri, String language) {
+        getRdfProperty (propertyUri, language)
+    }
+
+    private getRdfProperty (String propertyUri, String language) {
+
+        def property = ResourceFactory.createProperty (propertyUri)
+        def statements = jenaResource.listProperties (property).toList ()
+
+        def set = getPropertiesSet (statements, language)
+        if (set.size () > 1) {
+            return set
+        } else {
+            if (set.isEmpty ()) return null
+            return set.asList ().first ()
+        }
+    }
+
+    private getPropertiesSet (List statements, String language) {
+        def set = [] as Set
+        statements.each { Statement statement ->
+            def value = getLiteralValueOrResource (statement, language)
+            if (value) set += value
+        }
+        return set
+    }
+
+    private getLiteralValueOrResource (Statement statement, String language) {
+        def node = statement.getObject ()
+        if (node.isLiteral ()) {
+            def nodeLanguage = node.asLiteral ().getLanguage ()
+            if (!language || !nodeLanguage || nodeLanguage == language) {
+                return node.asLiteral ().getValue ()
+            }
+        } else {
+            return new JenaRdfResource (node.asResource ())
+        }
+    }
+
+    String toString () {
+        return uri
+    }
+
 }
